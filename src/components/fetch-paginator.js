da54c5b3a6dabf7ed89371474f69b0b97ab88c5d
Bihart <sanket.bihari@gmail.com>
2022-11-11 14:48:02 -0500

    [update] add logic and render to change the curr page.
    Signed-off-by: Bihart <sanket.bihari@gmail.com>

--- src/components/fetch-paginator.js
+++ src/components/fetch-paginator.js
@@ -4,33 +4,39 @@ import { PagePokemonRepository } from '../service/pagePokemonRepository';
 export class FetchPaginator extends LitElement {
     static properties = {
         pokemons: { type: Array },
-        nextUrl: { type: String },
-        prevUrl: { type: String },
-        currUrl: { type: String },
     }
 
     static styles = css`
-    :host {
+section {
     display: grid;
     grid-gap: 10px;
     grid-template: repeat(2, 1fr) / repeat(3, 0.5fr);
     grid-auto-flow: column;
 }`;
+    #homeUrl;
+    #nextUrl;
+    #prevUrl;
+    #currUrl;
     constructor() {
         super();
         this.pokemons = [];
-        this.home = "https://pokeapi.co/api/v2/pokemon?limit=6";
-        this.nextUrl = "";
-        this.prevUrl = "";
-        this.currUrl = "";
+        this.#homeUrl = "https://pokeapi.co/api/v2/pokemon?limit=6";
+        this.#nextUrl = "";
+        this.#prevUrl = "";
+        this.#currUrl = "";
+    }
+
+    async #getDataOfTheRepository(url) {
+        const pokemosAllDataRes = await PagePokemonRepository.get(url);
+        const { next, prev, pokemons } = pokemosAllDataRes;
+        this.pokemons = pokemons;
+        this.#currUrl = url;
+        this.#nextUrl = next;
+        this.#prevUrl = prev ?? "";
     }
 
     async firstUpdated() {
-        const pokemosAllDataRes = await PagePokemonRepository.get(this.home);
-        const { next, prev, pokemons } = pokemosAllDataRes;
-        this.pokemons = pokemons;
-        this.nextUrl = next;
-        this.prevUrl = prev;
+        await this.#getDataOfTheRepository(this.#homeUrl);
     }
 
     #mapPokemonsToHTML() {
@@ -39,9 +45,36 @@ export class FetchPaginator extends LitElement {
         );
     }
 
+    async #handleClick(event) {
+        const target = event.target;
+        const { tagName, value } = target;
+        const IsButton = tagName === "BUTTON";
+        if(!IsButton) {
+            return;
+        }
+        const urlsToGoMap = new Map([
+            ["prev", this.#prevUrl],
+            ["home", this.#homeUrl],
+            ["next", this.#nextUrl]
+        ]);
+        const urlToGo = urlsToGoMap.get(value);
+        await this.#getDataOfTheRepository(urlToGo);
+    }
+
     render() {
         return html`
+<section>
          ${this.#mapPokemonsToHTML()}
+</section>
+<section @click=${this.#handleClick}>
+${this.#prevUrl !== ""
+ ? html`<button value="prev">Go prev</button>`
+ : undefined}
+${this.#currUrl !== this.#homeUrl
+ ? html`<button value="home">Go home</button>`
+ : undefined}
+<button value="next">Go next</button>
+</section>
     `;
     }
 }
