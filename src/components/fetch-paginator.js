f27427359a61619222009b5b0220ed9a131a4807
Bihart <sanket.bihari@gmail.com>
2022-11-11 13:05:54 -0500

    [update] Add paginator props
    Necessary props to go to the next and prev page, these props do not yet
    have visula and logical trigger.
    
    Signed-off-by: Bihart <sanket.bihari@gmail.com>

--- src/components/fetch-paginator.js
+++ src/components/fetch-paginator.js
@@ -1,33 +1,47 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 import { PagePokemonRepository } from '../service/pagePokemonRepository';
 
 export class FetchPaginator extends LitElement {
     static properties = {
-        pokemons: { type: Array }
+        pokemons: { type: Array },
+        nextUrl: { type: String },
+        prevUrl: { type: String },
+        currUrl: { type: String },
     }
 
+    static styles = css`
+    :host {
+    display: grid;
+    grid-gap: 10px;
+    grid-template: repeat(2, 1fr) / repeat(3, 0.5fr);
+    grid-auto-flow: column;
+}`;
     constructor() {
         super();
         this.pokemons = [];
+        this.home = "https://pokeapi.co/api/v2/pokemon?limit=6";
+        this.nextUrl = "";
+        this.prevUrl = "";
+        this.currUrl = "";
     }
 
     async firstUpdated() {
-        const pokemosAllDataRes = await PagePokemonRepository.get();
-        this.pokemons = pokemosAllDataRes;
+        const pokemosAllDataRes = await PagePokemonRepository.get(this.home);
+        const { next, prev, pokemons } = pokemosAllDataRes;
+        this.pokemons = pokemons;
+        this.nextUrl = next;
+        this.prevUrl = prev;
     }
 
     #mapPokemonsToHTML() {
-        return this.pokemons.map((poke) => html`
-              <poke-card .pokemon=${poke}></poke-card>`
+        return this.pokemons.map(
+            (poke) => html`<poke-card .pokemon=${poke}></poke-card>`
         );
     }
 
-
     render() {
         return html`
-      <div>
          ${this.#mapPokemonsToHTML()}
-      </div>
     `;
     }
 }
