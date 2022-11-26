53ceae68c878e9e5c1820936fbeafab969dd5896
Bihart <sanket.bihari@gmail.com>
2022-11-26 13:32:26 -0500

    [style]
    `return true ? true : false` u.u
    
    Signed-off-by: Bihart <sanket.bihari@gmail.com>

--- src/icons/index.js
+++ src/icons/index.js
@@ -2,16 +2,14 @@ import { LitElement } from "lit";
 import { homeIcon, nextIcon, prevIcon } from "./paginator";
 
 export class Icon extends LitElement {
-  static properties = {
-    name: { type: String },
-  };
+    static properties = {
+        name: { type: String },
+    };
 
-  static icons = new Map([homeIcon, prevIcon, nextIcon]);
+    static icons = new Map([homeIcon, prevIcon, nextIcon]);
 
-  render() {
-    const icon = Icon.icons.get(this.name);
-
-    if (icon) return icon;
-    return "";
-  }
+    render() {
+        const icon = Icon.icons.get(this.name);
+        return icon ? icon : "";
+    }
 }
