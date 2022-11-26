a2bcfe6f2a4833cb8d9a065b9f716d574e1da98a
Bihart <sanket.bihari@gmail.com>
2022-11-26 13:34:31 -0500

    [fix] Now win the pokemon with less turn to win.
    Signed-off-by: Bihart <sanket.bihari@gmail.com>

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -6,7 +6,7 @@ class Pokemon_Battle {
         const { hp: hpP2, atk: atkP2} = pk2;
         const turnToWinP1 = Math.floor(hpP2 / atkP1);
         const turnToWinP2 = Math.floor(hpP1 / atkP2);
-        const winer = turnToWinP1 > turnToWinP2  ? pk1 : pk2;
+        const winer = turnToWinP1 < turnToWinP2  ? pk1 : pk2;
         const repo = new WinsPokemonRepository();
         repo.update(winer);
         return winer;
